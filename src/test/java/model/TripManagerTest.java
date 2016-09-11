4bf04535542197e1974fd926e00d15b98ab32bb3
alejandro11k <k11alejandro@gmail.com>
2016-09-11 17:27:29 -0300

    friendlytriptest unfinished

--- src/test/java/model/TripManagerTest.java
+++ src/test/java/model/TripManagerTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.mockito.Mockito.when;
 
@@ -38,5 +39,32 @@ public class TripManagerTest {
         when(mockProfile.allCategories()).thenReturn(new HashSet<Category>());
 
         Assert.assertEquals(1,tripManager.cheapTrip(eventsList,mockUser).size());
+    }
+
+    @Test
+    public void friendlyTrip(){
+        TripManager tripManager = new TripManager();
+        List<Event> eventsList = new ArrayList<>();
+        Set<Category> userCategories = new HashSet<Category>();
+        Set<Category> friendsCategories = new HashSet<Category>();
+        User mockUser = Mockito.mock(User.class);
+        Friends mockFriends = Mockito.mock(Friends.class);
+        Profile mockUserProfile = Mockito.mock(Profile.class);
+        when(mockUser.getProfile()).thenReturn(mockUserProfile);
+        when(mockUserProfile.allCategories()).thenReturn(userCategories);
+        when(mockUser.getFriends()).thenReturn(mockFriends);
+        when(mockFriends.allCategories()).thenReturn(friendsCategories);
+
+        Assert.assertEquals(0,tripManager.friendlyTrip(eventsList,mockUser).size());
+    }
+
+    @Test
+    public void surpriseTrip(){
+        TripManager tripManager = new TripManager();
+        Set<Event> allEvents = new HashSet<Event>();
+        //user //mock
+        //user.getFriends().categoriesOfUsersThatHaveAnyOfThis  //mock answer
+        //user.getProfile().allCategories()  //mock answer
+
     }
 }
